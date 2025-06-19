50ac68a9e745bc05c92ea0f067cd4eaa10b6b97d
ftmamhmod <ftmamhmod2@gmail.com>
2025-06-19 19:15:15 +0300

    refactor: remove Footer component and clean up layout structure

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,18 @@
-import Footer from "@/components/layout/footer";
 import Header from "@/components/layout/header";
 import LinesNumber from "@/components/layout/lines-numbers";
 import Sidebar from "@/components/layout/sidebar";
 import Terminal from "@/components/layout/terminal";
-import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="flex flex-col size-full">
       <Header />
@@ -16,18 +23,25 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
           <ResizablePanel defaultSize={91}>
             <div className="size-full flex overflow-hidden">
               <LinesNumber />
-              <div id="scroll" className="size-full overflow-y-auto overflow-x-hidden scroll-smooth">
+              <div
+                id="scroll"
+                className="size-full overflow-y-auto overflow-x-hidden scroll-smooth"
+              >
                 {children}
               </div>
             </div>
           </ResizablePanel>
           <ResizableHandle className="hidden sm:block" withHandle />
-          <ResizablePanel className="hidden sm:block" defaultSize={9} minSize={4} maxSize={25}>
+          <ResizablePanel
+            className="hidden sm:block"
+            defaultSize={9}
+            minSize={4}
+            maxSize={25}
+          >
             <Terminal />
           </ResizablePanel>
         </ResizablePanelGroup>
       </div>
-      <Footer />
     </div>
   );
 }
